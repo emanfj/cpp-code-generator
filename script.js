4e66b79921661cef0390a348a358f511e9642817
eman <syedaeman999@gmail.com>
2023-07-05 22:53:47 +0500

    properties included in the code blocks

--- script.js
+++ script.js
@@ -4,27 +4,25 @@ var dropArea = document.getElementById('canvas');
 //adding event handler for all draggable elements
 draggableElement.forEach((draggableElement) => {draggableElement.addEventListener('dragstart', dragStart)});
     
-//defining dragStart event handler
-function dragStart(event)
-{
-    const draggedData = generateCppCode(this.textContent); //using the dragged element's text content instead of assigning IDs
-    event.dataTransfer.setData('text/plain', draggedData) //generated cpp code is set as data to be transferred 
+function dragStart(event) {
+    const draggedData = generateCppCode(this.textContent.trim());
+    event.dataTransfer.setData('text/plain', JSON.stringify(draggedData));
 }
+
 
 //add event listeners for drop functionality
 dropArea.addEventListener('drop',drop);
 dropArea.addEventListener('dragover',dragOver);
 dropArea.addEventListener('dragleave', dragLeave); 
-
-//when element is dropped within the target
-function drop(event) 
-{
-    //prevent the default behaviour for dropped elements
+// when element is dropped within the target
+function drop(event) {
+    // prevent the default behavior for dropped elements
     event.preventDefault();
-    dropArea.classList.remove('dragover'); //remove the 'dragover' class
-    //retrieve dropped element's data
-    var droppedData = event.dataTransfer.getData('text/plain');
-    
+    dropArea.classList.remove('dragover'); // remove the 'dragover' class
+
+    // retrieve dropped element's data
+    var { code, properties } = JSON.parse(event.dataTransfer.getData('text/plain'));
+
     // create the code block container
     const codeBlock = document.createElement('div');
     codeBlock.className = 'code-block';
@@ -41,60 +39,77 @@ function drop(event)
     const plusSign = document.createElement('span');
     plusSign.innerHTML = '&#43;'; // Unicode for plus symbol
     plusSign.className = 'plus-sign';
-    //upon click add function later that would add the code block to compiler region
+    // upon click, add function later that would add the code block to the compiler region
 
     // create the code text
     const codeText = document.createElement('span');
-    codeText.textContent = droppedData;
+    codeText.textContent = code;
 
-    //create the properties dropdown
-    const propertiesList = document.createElement('ul');
-    propertiesList.className = 'properties-list';
+    // create the properties form
+    const propertiesForm = document.createElement('form');
+    propertiesForm.className = 'properties-list';
 
-    //create list items for the properties
-    const nameItem = document.createElement('li');
-    nameItem.textContent = 'Name';
-    nameItem.className = 'property-item';
-
-    const initialValueItem = document.createElement('li');
-    initialValueItem.textContent = 'Initial Value';
-    initialValueItem.className = 'property-item';
-
-    const dataTypeItem = document.createElement('li');
-    dataTypeItem.textContent = 'Data Type';
-    dataTypeItem.className = 'property-item';
-
-    const dimensionItem = document.createElement('li');
-    dimensionItem.textContent = 'Dimension';
-    dimensionItem.className = 'property-item';
-    
-
-    //hide properties list by default
-    propertiesList.style.display = 'none';
-
-    //toggle visibility of properties list for code block
-    codeBlock.addEventListener('click',function()
+    //create input fields for the properties 
+    properties.forEach((property) => 
     {
-        propertiesList.style.display=propertiesList.style.display === 'none' ? 'block' : 'none';
-    }); 
+        const inputField = document.createElement('input');
+        inputField.type = 'text';
+        inputField.placeholder = property;
+        inputField.className = 'property-input'; 
+        //append the input field to the properties form 
+        propertiesForm.appendChild(inputField);
+    });
 
-    //append list items to proprties list
-    propertiesList.appendChild(nameItem);
-    propertiesList.appendChild(initialValueItem);
-    propertiesList.appendChild(dataTypeItem);
-    propertiesList.appendChild(dimensionItem);
+
+    // const dimensionInput = document.createElement('select');
+    // dimensionInput.className = 'dropdown-input';
+    // // dimensionInput.name = 'Dimension';
+
+    // //default placeholder option (not selectable)
+    // const placeholderOption = document.createElement('option');
+    // placeholderOption.disabled = true;
+    // placeholderOption.selected = true;
+    // placeholderOption.textContent = 'Dimension'; // placeholder text
+    // placeholderOption.className = 'placeholder-option'; // add a CSS class
+    // dimensionInput.appendChild(placeholderOption);
+
+    // // populate the dimension dropdown with options
+    // const dimensionOptions = ['1D','2D', '3D'];
+    // dimensionOptions.forEach(function (option) {
+    //     const optionElement = document.createElement('option');
+    //     optionElement.value = option;
+    //     optionElement.textContent = option;
+    //     dimensionInput.appendChild(optionElement);
+    // });
+
+    // hide properties form by default
+    propertiesForm.style.display = 'none';
+
+    // toggle visibility of properties form for code block
+    codeBlock.addEventListener('click', function (event) {
+    event.stopPropagation(); // Prevents the event from propagating to the document click event
+    propertiesForm.style.display = 'block';
+    });
+
+    // hide properties form when clicked anywhere outside the code block or properties form
+    document.addEventListener('click', function (event) {
+        const target = event.target;
+        if (!codeBlock.contains(target) && target !== propertiesForm) {
+            propertiesForm.style.display = 'none';
+        }
+    });
+
 
     // append the elements to the code block container
     codeBlock.appendChild(codeText);
-    codeBlock.appendChild(propertiesList);
+    codeBlock.appendChild(propertiesForm);
     codeBlock.appendChild(deleteIcon);
     codeBlock.appendChild(plusSign);
-    
 
     // append the code block to the drop target
     dropArea.appendChild(codeBlock);
-
 }
+
  
 //when the element is being dragged within the boundary of the drop target
 function dragOver(event)
@@ -112,42 +127,42 @@ function dragLeave(event)
 let vCounter = 1;
 let functionCounter = 1;
 let arrayCounter = 1;
+
 function generateCppCode(menuItem) {
-  let cppCode = '';
-
-  switch (menuItem) {
-    case 'int':
-    case 'float':
-    case 'bool':
-    case 'char':
-    case 'string':
-
-        let variableName = `v${vCounter}`; //string interpolation to embed variables to create strings
-        cppCode = `${menuItem} ${variableName}`
-        //update the counter for the variable name
+    let cppCode = '';
+    let properties = [];
+  
+    switch (menuItem) {
+      case 'Variable':
+        let variableName = `v${vCounter}`;
+        cppCode = `dataType ${variableName}`;
+        properties = ['Name', 'Initial Value', 'Data Type', 'Dimension'];
         vCounter += 1;
-
         break;
-
-    case 'for':
-        cppCode = `for (int i = 0; i < n; i++) { //loop logic }`;
+  
+      case 'for':
+        cppCode = `for (int i = 0; i < n; i++) { // loop logic }`;
+        properties = ['Initial Value \'i' ,'Condition \'n'];
         break;
 
     case 'while':
         cppCode = `while (condition) { //loop logic }`;
+        properties = ['Condition'];
         break;
 
     case 'do while':
         cppCode = `do { //loop logic } while (condition);`;
+        properties = ['Condition'];
         break;
-
+          
     case 'Function':
         let functionName = `func${functionCounter}`;
-        cppCode = `returnType ${functionName}(parameters) { // function implementation }`; // replace "returnType" and "parameters" with actual values
+        cppCode = `returnType ${functionName}(parameters) { // function implementation }`;
+        properties = ['Function Name', 'Return Type'];
         functionCounter += 1;
         break;
-
-    //arithmetic operators
+    
+        //arithmetic operators
     case '+':
     case '-':
     case 'x':
@@ -179,7 +194,7 @@ function generateCppCode(menuItem) {
     case '*':
         cppCode = `${menuItem}`;
         break;
-
+    
     case 'if':
       cppCode = 'if (condition) {\n\t// code block\n}';
       break;
@@ -205,29 +220,32 @@ function generateCppCode(menuItem) {
       break;
 
     case 'Array':
-        let arrayName = `arr${arrayCounter}`;
-        cppCode = `type ${arrayName}[size];`; // replace "type" with the actual array data type
-        arrayCounter += 1;
-        break;
-    
+      let arrayName = `arr${arrayCounter}`;
+      cppCode = `type ${arrayName}[size];`;
+      properties = ['Name', 'Data Type', 'Size', 'Dimension'];
+      arrayCounter += 1;
+      break;
+
     case 'cout':
-        cppCode = 'cout';
-        break;
+      cppCode = 'cout';
+      break;
 
     case 'cin':
-        cppCode = 'cin';
-        break;
+      cppCode = 'cin';
+      break;
 
     case 'getline':
-        cppCode = 'getline(cin, variableName);';
-        break;    
-
-    // Add more cases for other menu items
-
-    default:
-      cppCode = ''; //if the menuItem doesn't match any case, cppCode remains an empty string
+      cppCode = 'getline(cin, variableName);';
+      properties = ['Variable Name'];
       break;
-  }
+    
+    default:
+        cppCode = '';
+        properties = [];
+        break;
+    }
+  
 
-  return cppCode;
-}
+
+    return { code: cppCode, properties };
+  }
