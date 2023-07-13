cf7791e63e7672a0aab69efe8ac9367df8a8fa47
eman <syedaeman999@gmail.com>
2023-07-13 09:37:30 +0500

    code editor included

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
 var draggableElement = document.querySelectorAll('.draggable');	
 var dropArea = document.getElementById('canvas');
 
+var editor = ace.edit("codeEditor"); //ace editor instance
+
+ace.require("ace/ext/language_tools");  //enable the language tools extension
+
+//set the initial content
+editor.setValue("#include <iostream>\nusing namespace std;\n\nint main() {\n\n\n}");
+
+editor.setOptions({
+   showLineNumbers: true, 
+   theme: "ace/theme/chrome", 
+   mode: "ace/mode/c_cpp",
+   fontSize: "16px",
+   enableBasicAutocompletion: true,
+   enableLiveAutocompletion: true,
+   showGutter: true,
+   highlightActiveLine: true,
+   bracketMatching: true
+ });
+
 //adding event handler for all draggable elements
 draggableElement.forEach((draggableElement) => {draggableElement.addEventListener('dragstart', dragStart)});
     
@@ -13,68 +32,75 @@ function dragStart(event) {
 dropArea.addEventListener('drop',drop);
 dropArea.addEventListener('dragover',dragOver);
 dropArea.addEventListener('dragleave', dragLeave); 
+// Create a variable to store the properties of the code block
+let currentProperties = {};
 
 // when element is dropped within the target
 function drop(event) {
-    // prevent the default behavior for dropped elements
-    event.preventDefault();
-    dropArea.classList.remove('dragover'); // remove the 'dragover' class
+  // prevent the default behavior for dropped elements
+  event.preventDefault();
+  dropArea.classList.remove('dragover'); // remove the 'dragover' class
 
-    // retrieve dropped element's data
-    var { code, properties } = JSON.parse(event.dataTransfer.getData('text/plain'));
+  // retrieve dropped element's data
+  var { code, properties } = JSON.parse(event.dataTransfer.getData('text/plain'));
 
-    // create the code block container
-    const codeBlock = document.createElement('div');
-    codeBlock.className = 'code-block';
+  // Store the properties in the currentProperties variable
+  currentProperties = properties;
 
-    // create the delete icon
-    const deleteIcon = document.createElement('span');
-    deleteIcon.innerHTML = '<i class="fas fa-trash-alt"></i>';
-    deleteIcon.className = 'delete-icon';
-    deleteIcon.addEventListener('click', function () {
-        codeBlock.remove();
-    });
+  // create the code block container
+  const codeBlock = document.createElement('div');
+  codeBlock.className = 'code-block';
 
-    // create the plus sign
-    const plusSign = document.createElement('span');
-    plusSign.innerHTML = '&#43;'; // Unicode for plus symbol
-    plusSign.className = 'plus-sign';
-    // upon click, add function later that would add the code block to the compiler region
+  // create the delete icon
+  const deleteIcon = document.createElement('span');
+  deleteIcon.innerHTML = '<i class="fas fa-trash-alt"></i>';
+  deleteIcon.className = 'delete-icon';
+  deleteIcon.addEventListener('click', function () {
+    codeBlock.remove();
+  });
 
-    // create the code text
-    const codeText = document.createElement('span');
-    codeText.textContent = code;
+  // create the plus sign
+  const plusSign = document.createElement('span');
+  plusSign.innerHTML = '&#43;'; // Unicode for plus symbol
+  plusSign.className = 'plus-sign';
+  plusSign.addEventListener('click', function () {
+    // updateCodeEditor(code, currentProperties);
 
-    // create the properties form
-    const propertiesForm = document.createElement('form');
-    propertiesForm.className = 'properties-list';
+  });
 
-    // create input fields for the properties
-    properties.forEach((property) => {
+  // create the code text
+  const codeText = document.createElement('span');
+  codeText.textContent = code;
+
+  // create the properties form
+  const propertiesForm = document.createElement('form');
+  propertiesForm.className = 'properties-list';
+
+  // create input fields for the properties
+  properties.forEach((property) => {
     let inputField;
-  
+
     if (property === 'Data Type' || property === 'Return Type' || property === 'Dimension') {
       //select input for specific properties
       inputField = document.createElement('select');
       inputField.className = 'dropdown-input';
-  
+
       //add default placeholder option (not selectable)
       const placeholderOption = document.createElement('option');
       placeholderOption.disabled = true;
       placeholderOption.selected = true;
       placeholderOption.textContent = property;
-      placeholderOption.className = 'placeholder-option'; 
+      placeholderOption.className = 'placeholder-option';
       inputField.appendChild(placeholderOption);
-  
+
       //adding options based on property type
       if (property === 'Data Type') {
-        ['int', 'float','bool','char','string'].forEach((option) => {
+        ['int', 'float', 'bool', 'char', 'string'].forEach((option) => {
           const optionElement = document.createElement('option');
           optionElement.value = option;
           optionElement.textContent = option;
           inputField.appendChild(optionElement);
         });
-  
       } else if (property === 'Return Type') {
         ['void', 'int'].forEach((option) => {
           const optionElement = document.createElement('option');
@@ -82,14 +108,13 @@ function drop(event) {
           optionElement.textContent = option;
           inputField.appendChild(optionElement);
         });
-  
       } else if (property === 'Dimension') {
-          const dimensionOptions = ['1D','2D', '3D'];
-          dimensionOptions.forEach(function (option) {
-            const optionElement = document.createElement('option');
-            optionElement.value = option;
-            optionElement.textContent = option;
-            inputField.appendChild(optionElement);
+        const dimensionOptions = ['1D', '2D', '3D'];
+        dimensionOptions.forEach(function (option) {
+          const optionElement = document.createElement('option');
+          optionElement.value = option;
+          optionElement.textContent = option;
+          inputField.appendChild(optionElement);
         });
       }
     } else {
@@ -99,39 +124,37 @@ function drop(event) {
       inputField.placeholder = property;
       inputField.className = 'property-input';
     }
-  
-    
+
     propertiesForm.appendChild(inputField);
   });
 
+  // hide properties form by default
+  propertiesForm.style.display = 'none';
 
-    // hide properties form by default
-    propertiesForm.style.display = 'none';
-
-    // toggle visibility of properties form for code block
-    codeBlock.addEventListener('click', function (event) {
+  // toggle visibility of properties form for code block
+  codeBlock.addEventListener('click', function (event) {
     event.stopPropagation(); // Prevents the event from propagating to the document click event
     propertiesForm.style.display = 'block';
-    });
+  });
 
-    // hide properties form when clicked anywhere outside the code block or properties form
-    document.addEventListener('click', function (event) {
-        const target = event.target;
-        if (!codeBlock.contains(target) && target !== propertiesForm) {
-            propertiesForm.style.display = 'none';
-        }
-    });
+  // hide properties form when clicked anywhere outside the code block or properties form
+  document.addEventListener('click', function (event) {
+    const target = event.target;
+    if (!codeBlock.contains(target) && target !== propertiesForm) {
+      propertiesForm.style.display = 'none';
+    }
+  });
 
+  // append the elements to the code block container
+  codeBlock.appendChild(codeText);
+  codeBlock.appendChild(propertiesForm);
+  codeBlock.appendChild(deleteIcon);
+  codeBlock.appendChild(plusSign);
 
-    // append the elements to the code block container
-    codeBlock.appendChild(codeText);
-    codeBlock.appendChild(propertiesForm);
-    codeBlock.appendChild(deleteIcon);
-    codeBlock.appendChild(plusSign);
-
-    // append the code block to the drop target
-    dropArea.appendChild(codeBlock);
+  // append the code block to the drop target
+  dropArea.appendChild(codeBlock);
 }
+
 
  
 //when the element is being dragged within the boundary of the drop target
@@ -270,3 +293,32 @@ function generateCppCode(menuItem) {
   
     return { code: cppCode, properties };
   }
+
+  // function updateCodeEditor(code, properties) {
+  //   let updatedCode = code;
+  
+  //   // Iterate over the properties and update the code accordingly
+  //   properties.forEach((property) => {
+  //     // Retrieve the value of the property from the corresponding input field
+  //     const inputField = propertiesForm.querySelector(`[placeholder="${property}"]`);
+  //     const propertyValue = inputField.value;
+  
+  //     // Replace the placeholder in the code with the actual value
+  //     updatedCode = updatedCode.replace(property, propertyValue);
+  //   });
+  
+  //   // Concatenate the updated code from all code blocks
+  //   const codeBlocks = document.querySelectorAll('.code-block');
+  //   let finalCode = '';
+    
+  //   codeBlocks.forEach((codeBlock) => {
+  //     finalCode += codeBlock.querySelector('span').textContent + '\n';
+  //   });
+  
+  //   // Replace the original code with the updated code
+  //   finalCode = finalCode.replace(code, updatedCode);
+  
+  //   // Update CodeMirror editor content
+  //   editor.setValue(finalCode);
+  // }
+  
